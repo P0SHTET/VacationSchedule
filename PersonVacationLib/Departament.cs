ccd483b5a53d1ec7870ecd83b692d7e6bd2d5de8
galoperidol08@gmail.com <galoperidol08@gmail.com>
2021-07-08 21:35:36 +1000

    add person classes

---
+++ PersonVacationLib/Departament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonVacationLib
+{
+    class Departament
+    {
+        private string _name;
+        private List<PersonVacation> _personVacations;
+
+    }
+}
---
+++ PersonVacationLib/PersonVacation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonVacationLib
+{
+    public class PersonVacation
+    {
+        private string _name;
+        private DateTime _startVacation;
+        private DateTime _endVacation;
+
+    }
+}
