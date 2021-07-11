61682b674e5277ee5d46ce1b08d013c9b87bc39e
galoperidol08@gmail.com <galoperidol08@gmail.com>
2021-07-11 17:29:40 +1000

    fix user controls

--- VacationSchedule/WorkersVacation.xaml.cs
+++ VacationSchedule/WorkersVacation.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VacationSchedule.VacationUserControl
+namespace VacationSchedule
 {
     /// <summary>
     /// Логика взаимодействия для WorkersVacation.xaml
